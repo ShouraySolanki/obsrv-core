4448cebb814b0d5ca5141020824a0d52e5a38bcf
yravinderkumar33 <yravinderkumar33@gmail.com>
2023-04-27 15:15:59 +0530

    test automation suite - master dataset payload

--- test-automation/src/config/index.ts
+++ test-automation/src/config/index.ts
@@ -3,8 +3,8 @@ import _ from 'lodash';
 const env = process.env;
 
 export default {
-    OBS_API_SERVICE: _.get(env, 'OBS_API_SERVICE') || "http://localhost:4000",
-    PROMETHEUS: _.get(env, 'PROMETHEUS') || "http://localhost:9090",
+    OBS_API_SERVICE: _.get(env, 'obs_api_service_host') || "http://localhost:4000",
+    PROMETHEUS: _.get(env, 'prometheus') || "http://localhost:9090",
     POSTGRES_HOST: _.get(env, 'postgres_host') || 'localhost',
     POSTGRES_PORT: _.get(env,'postgres_port' ) || "5432",
     POSTGRES_USERNAME: _.get(env, 'postgres_username' ) || 'obsrv',
