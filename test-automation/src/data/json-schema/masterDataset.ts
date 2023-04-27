a4f2ba1cce7ee03da799e9730e23b33101fefc30
yravinderkumar33 <yravinderkumar33@gmail.com>
2023-04-27 15:15:59 +0530

    test automation suite - master dataset payload

--- test-automation/src/data/json-schema/masterDataset.ts
+++ test-automation/src/data/json-schema/masterDataset.ts
@@ -1,24 +1,26 @@
 export default {
-    "$schema": "https://json-schema.org/draft/2020-12/schema",
-    "type": "object",
-    "properties": {
-        "id": {
-            "type": "string",
-            "format": "uuid",
-            "suggestions": [
-                {
-                    "message": "The Property 'id' appears to be 'uuid' format type.",
-                    "advice": "Suggest to not to index the high cardinal columns",
-                    "resolutionType": "DEDUP",
-                    "severity": "LOW"
-                }
-            ]
-        },
-        "assetName": {
-            "type": "string"
-        },
-        "assetLocation": {
-            "type": "string"
+    "observations": {
+        "$schema": "https://json-schema.org/draft/2020-12/schema",
+        "type": "object",
+        "properties": {
+            "id": {
+                "type": "string",
+                "format": "uuid",
+                "suggestions": [
+                    {
+                        "message": "The Property 'id' appears to be 'uuid' format type.",
+                        "advice": "Suggest to not to index the high cardinal columns",
+                        "resolutionType": "DEDUP",
+                        "severity": "LOW"
+                    }
+                ]
+            },
+            "assetName": {
+                "type": "string"
+            },
+            "assetLocation": {
+                "type": "string"
+            }
         }
     }
 }
