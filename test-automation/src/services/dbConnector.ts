d8a0426335c79a70664c49e629994ad775b4639b
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2023-04-27 13:18:55 +0530

    creating tables

---
+++ test-automation/src/services/dbConnector.ts
@@ -0,0 +1,18 @@
+import { createQueries } from "../data/tables";
+import { Sequelize } from "sequelize"
+
+export const createTables = async () => {
+    const sequelize = new Sequelize('postgres://postgres:postgres@localhost:5432/test');
+    for (const tableSqlObject of createQueries) {
+        const tableName: string = Object.keys(tableSqlObject)[0];
+        const sqlQuery: string = Object.values(tableSqlObject)[0];
+        try {
+            await sequelize.query(sqlQuery);
+            console.log(`Table ${tableName} created successfully`);
+        } catch (err) {
+            console.error(`Failed to create table ${tableName}: ${err}`);
+        } finally {
+        }
+    }
+    sequelize.close()
+}
--- test-automation/src/tasks/createTables.ts
+++ test-automation/src/tasks/createTables.ts
@@ -1,9 +1,10 @@
+import { createTables } from "../services/dbConnector";
 export default {
     index: 1,
     name: 'createTables',
     handler: (payloadFromPreviousTasks: Record<string, any>) => {
         return async () => {
-
+            await createTables()
         }
     }
-};
+}
