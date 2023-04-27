0fb50cb1e39bf12cb189faabe4bc615ca5ff4477
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2023-04-27 18:33:27 +0530

    test event genrations

--- test-automation/src/tasks/pushEventsToDataset.ts
+++ test-automation/src/tasks/pushEventsToDataset.ts
@@ -1,9 +1,12 @@
+const { pushObsEvents } = require('../utils/obs')
+export const observationsDataset = "observations-transformed"
+
 export default {
     index: 1,
     name: 'pushEventsToDataset',
     handler: (payloadFromPreviousTasks: Record<string, any>) => {
         return async () => {
-            
+            await pushObsEvents(100)
         }
     }
-};
+}
