093beda6081e61837b584944fdc6d61ec8cfbef5
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2023-04-27 19:19:24 +0530

    Bug fix in creating tables

--- test-automation/src/services/dbConnector.ts
+++ test-automation/src/services/dbConnector.ts
@@ -1,25 +1,27 @@
 import { createQueries } from "../data/tables";
 import { Sequelize } from "sequelize"
-import * as config from '../config' 
+import * as config from '../config'
 export const createTables = async () => {
-    const sequelize = new Sequelize({
-        dialect: 'postgres',
-        host: config.default.POSTGRES_HOST,
-        port: parseInt(config.default.POSTGRES_PORT),
-        username: config.default.POSTGRES_USERNAME,
-        password: config.default.POSTGRES_PASSWORD,
-        database: config.default.POSTGRES_DATABASE
-    });
-    for (const tableSqlObject of createQueries) {
-        const tableName: string = Object.keys(tableSqlObject)[0];
-        const sqlQuery: string = Object.values(tableSqlObject)[0];
-        try {
+    try {
+        const sequelize = new Sequelize({
+            dialect: 'postgres',
+            host: config.default.POSTGRES_HOST,
+            port: parseInt(config.default.POSTGRES_PORT),
+            username: config.default.POSTGRES_USERNAME,
+            password: config.default.POSTGRES_PASSWORD,
+            database: config.default.POSTGRES_DATABASE
+        });
+        for (const tableSqlObject of createQueries) {
+            const tableName: string = Object.keys(tableSqlObject)[0];
+            const sqlQuery: string = Object.values(tableSqlObject)[0];
+
             await sequelize.query(sqlQuery);
-            return true;
-        } catch (err) {
-            console.error(`Failed to create table ${tableName}: ${err}`);
-            throw err;
+
         }
+        sequelize.close()
+        return true;
+    } catch (err) {
+        console.error(`Failed to create tables: ${err}`);
+        throw err;
     }
-    sequelize.close()
 }
