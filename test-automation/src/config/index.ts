2e7f1b955013558fae4155a1a268f08e421c4797
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2023-04-27 13:37:56 +0530

    getting dbconnector config from ennv

--- test-automation/src/config/index.ts
+++ test-automation/src/config/index.ts
@@ -6,7 +6,7 @@ export default {
     OBS_API_SERVICE: _.get(env, 'OBS_API_SERVICE') || "http://localhost:4000",
     PROMETHEUS: _.get(env, 'PROMETHEUS') || "http://localhost:9090",
     POSTGRES_HOST: _.get(env, 'postgres_host') || 'localhost',
-    POSTGRES_PORT: _.get(env,'postgres_port' ) || 5432,
+    POSTGRES_PORT: _.get(env,'postgres_port' ) || "5432",
     POSTGRES_USERNAME: _.get(env, 'postgres_username' ) || 'obsrv',
     POSTGRES_PASSWORD: _.get(env, 'postgres_password') || '5b-0b5rv',
     POSTGRES_DATABASE: _.get(env, 'postgres_database' ) || 'sb-obsrv',
--- test-automation/src/services/dbConnector.ts
+++ test-automation/src/services/dbConnector.ts
@@ -1,8 +1,15 @@
 import { createQueries } from "../data/tables";
 import { Sequelize } from "sequelize"
-
+import * as config from '../config' 
 export const createTables = async () => {
-    const sequelize = new Sequelize('postgres://postgres:postgres@localhost:5432/test');
+    const sequelize = new Sequelize({
+        dialect: 'postgres',
+        host: config.default.POSTGRES_HOST,
+        port: parseInt(config.default.POSTGRES_PORT),
+        username: config.default.POSTGRES_USERNAME,
+        password: config.default.POSTGRES_PASSWORD,
+        database: config.default.POSTGRES_DATABASE
+    });
     for (const tableSqlObject of createQueries) {
         const tableName: string = Object.keys(tableSqlObject)[0];
         const sqlQuery: string = Object.values(tableSqlObject)[0];
