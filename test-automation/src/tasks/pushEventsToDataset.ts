71ceb927f97ebe84dd7e78e676db81fdbc177583
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2023-05-02 13:34:53 +0530

    added topic names

--- test-automation/src/tasks/pushEventsToDataset.ts
+++ test-automation/src/tasks/pushEventsToDataset.ts
@@ -1,11 +1,11 @@
 const { pushObsEvents } = require('../utils/obs')
-
+export const batchSize = 100
 export default {
     index: 1,
     name: 'pushEventsToDataset',
     handler: (payloadFromPreviousTasks: Record<string, any>) => {
         return async () => {
-            await pushObsEvents(1)
+            await pushObsEvents(batchSize)
         }
     }
 }
