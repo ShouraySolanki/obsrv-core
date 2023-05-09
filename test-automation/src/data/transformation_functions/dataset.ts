4dc4600633a78c635c625ff31abed54ebbdecdcf
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2023-05-09 18:02:28 +0530

    moved automation test suite to automation-ext

--- test-automation/src/data/transformation_functions/dataset.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    "observations": {
-        "mask": { "type": "mask", "condition": null, "expr": "spatialExtent" },
-        "encrypt": { "type": "encrypt", "condition": null, "expr": "yMax" },
-        "jsonata_timestampString": { "type": "jsonata", "condition": null, "expr": "$fromMillis(modified)" },
-        "jsonata_codeComponentsCount": { "type": "jsonata", "condition": null, "expr": "$count(codeComponents)" },
-        "jsonata_codeComponentValues": { "type": "jsonata", "condition": null, "expr": "codeComponents.value" }
-    }
-}
--- test-automation/src/types/index.ts
+++
@@ -1,6 +0,0 @@
-export interface ITask {
-    index: number,
-    name: string;
-    handler: (config: Record<string, any>) => Function;
-    [key: string]: any;
-}
